da8d0ec448b87f2d629d513aa7213b32ca546008
kirill kurenev <cirill@mail.ru>
2022-09-17 20:38:21 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -3,6 +3,17 @@
 M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 M = 4; N = 8. -> ""4, 6, 7, 8""
 */
+string PrintNumber (int m, int n)
+{
+   if (m == n) return $"{n}";
+   return $"{m} {PrintNumber(m+1, n)}";
+}
+Console.WriteLine("Задача 1");
+Console.Write("Введите начало отчета ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конец ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumber(m, n));
 /*
 Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
